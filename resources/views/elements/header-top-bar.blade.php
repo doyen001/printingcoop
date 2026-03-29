7d0c225d96f3377980c69fd4c10079408296cde9
Heorh <hmykhailiuk1007@gmail.com>
2026-03-29 23:11:29 +0200

    change flag svg

--- resources/views/elements/header-top-bar.blade.php
+++ resources/views/elements/header-top-bar.blade.php
@@ -390,11 +390,25 @@
                                 <li style="display: flex">
                                     @if(!empty($StoreListData) && is_array($StoreListData))
                                         @foreach($StoreListData as $key => $language)
-                                            <a href="{{ $language['url'] ?? '#' }}">
+                                            <a href="{{ $language['url'] ?? '#' }}" style="display: flex; align-items: center;">
                                                 @if($language['language_name'] == 'English') 
-                                                    <span style="margin-right: 4px; font-family: Arial, sans-serif;">&#127468;&#127463;</span>EN
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 30" style="width: 20px; height: 14px; margin-right: 4px;">
+                                                        <clipPath id="s"><path d="M0,0 v30 h60 v-30 z"/></clipPath>
+                                                        <clipPath id="t"><path d="M30,15 h30 v15 z v15 h-30 z h-30 v-15 z v-15 h30 z"/></clipPath>
+                                                        <g clip-path="url(#s)">
+                                                            <path d="M0,0 v30 h60 v-30 z" fill="#012169"/>
+                                                            <path d="M0,0 L60,30 M60,0 L0,30" stroke="#fff" stroke-width="6"/>
+                                                            <path d="M0,0 L60,30 M60,0 L0,30" clip-path="url(#t)" stroke="#C8102E" stroke-width="4"/>
+                                                            <path d="M30,0 v30 M0,15 h60" stroke="#fff" stroke-width="10"/>
+                                                            <path d="M30,0 v30 M0,15 h60" stroke="#C8102E" stroke-width="6"/>
+                                                        </g>
+                                                    </svg>EN
                                                 @else 
-                                                    <span style="margin-right: 4px; font-family: Arial, sans-serif;">&#127467;&#127479;</span>FR
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 900 600" style="width: 20px; height: 14px; margin-right: 4px;">
+                                                        <rect width="900" height="600" fill="#ED2939"/>
+                                                        <rect width="600" height="600" fill="#fff"/>
+                                                        <rect width="300" height="600" fill="#002395"/>
+                                                    </svg>FR
                                                 @endif
                                             </a>
                                             @if(!$loop->last)
